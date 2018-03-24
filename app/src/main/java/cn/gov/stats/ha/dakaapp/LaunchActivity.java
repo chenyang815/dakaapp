10ebac13a331dbc328bac71771c13ae4e7dbba41
sukidesu <hxysir@gmail.com>
2018-03-24 21:01:33 +0800

    3 commit

--- app/src/main/java/cn/gov/stats/ha/dakaapp/LaunchActivity.java
+++ app/src/main/java/cn/gov/stats/ha/dakaapp/LaunchActivity.java
@@ -3,16 +3,20 @@ package cn.gov.stats.ha.dakaapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
-public class LaunchActivity extends Activity {
+public class LaunchActivity extends Activity implements View.OnClickListener{
+    private static final int LOGIN_JUDGE = 1;
     private EditText mAccount;
     private EditText mPassword;
     private Button mLogin;
+    private final static int REGISTER_JUDGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,24 +27,68 @@ public class LaunchActivity extends Activity {
         mPassword = (EditText)findViewById(R.id.password);
         mLogin = (Button)findViewById(R.id.login);
 
-        mLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                final String id = mAccount.getText().toString();
-                final String password = mPassword.getText().toString();
-
-                if(id.equals("18538721898")&password.equals("123456")){
-                    Intent intent = new Intent(LaunchActivity.this,MainActivity.class);
-                    intent.putExtra("id1",id);
-                    startActivity(intent);
-                }else {
-                    Toast.makeText(LaunchActivity.this,"登陆失败，密码错误",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        mLogin.setOnClickListener(this);
 
     }
 
 
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            super.handleMessage(msg);
+            switch (msg.what){
+                case LOGIN_JUDGE:{
+                    Bundle bundle = new Bundle();
+                    bundle = msg.getData();
+                    String result = bundle.getString("result");
+                    try {
+                        if (result.equals("success")) {
+                            Toast.makeText(LaunchActivity.this,"登录成功！",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LaunchActivity.this,MainActivity.class);
+                            startActivity(intent);
+                        }
+                    }catch (NullPointerException e){
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            }
+        }
+    };
+
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.login:{
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //使用下面类里的函数，连接servlet，返回一个result，使用handler处理这个result
+                        String result = HttpLogin.LoginByPost(mAccount.getText().toString(),mPassword.getText().toString());
+                        Bundle bundle = new Bundle();
+                        bundle.putString("result",result);
+                        Message message = new Message();
+                        message.setData(bundle);
+                        message.what = LOGIN_JUDGE;
+                        handler.sendMessage(message);
+                    }
+                }).start();
+            }
+            break;
+        }
+//        final String id = mAccount.getText().toString();
+//        final String password = mPassword.getText().toString();
+//
+//        if(id.equals("18538721898")&password.equals("123456")){
+//            Intent intent = new Intent(LaunchActivity.this,MainActivity.class);
+//            intent.putExtra("id1",id);
+//            startActivity(intent);
+//        }else {
+//            Toast.makeText(LaunchActivity.this,"登陆失败，密码错误",Toast.LENGTH_SHORT).show();
+//        }
+
+    }
 }
--- app/src/main/java/cn/gov/stats/ha/dakaapp/MainActivity.java
+++ app/src/main/java/cn/gov/stats/ha/dakaapp/MainActivity.java
@@ -3,14 +3,19 @@ package cn.gov.stats.ha.dakaapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.net.URL;
 
 public class MainActivity extends Activity implements View.OnClickListener{
     private String id;
     private String url;
+
+    private static final int DAKASUCC = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,24 +24,92 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
         Intent intent = getIntent();
         id = intent.getStringExtra("id1");
-        url = "http://192.168.1.101:8080/tjjdaka/MyServlet";
+        url = "http://192.168.1.101:8080/tjjdaka/DakaServlert";
     }
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            super.handleMessage(msg);
+            switch (msg.what){
+                case DAKASUCC:{
+                    Bundle bundle = new Bundle();
+                    bundle = msg.getData();
+                    String result = bundle.getString("result");
+                    try {
+                        if (result.equals("succ")) {
+                            Toast.makeText(MainActivity.this,"打卡成功！",Toast.LENGTH_SHORT).show();
+                        }
+                    }catch (NullPointerException e){
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            }
+        }
+    };
+
+
 
     @Override
     public void onClick(View view) {
 
         switch (view.getId()){
             case R.id.morcome:
-                new HttpThread(url,id,"morcame").start();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String result = HttpDaka.DakaByPost("18538721898","morcome");
+                        Bundle bundle = new Bundle();
+                        bundle.putString("result",result);
+                        Message message = new Message();
+                        message.setData(bundle);
+                        message.what = DAKASUCC;
+                        handler.sendMessage(message);
+                    }
+                }).start();
                 break;
             case R.id.morleave:
-                new HttpThread(url,id,"morleave").start();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String result = HttpDaka.DakaByPost("18538721898","morleave");
+                        Bundle bundle = new Bundle();
+                        bundle.putString("result",result);
+                        Message message = new Message();
+                        message.setData(bundle);
+                        message.what = DAKASUCC;
+                        handler.sendMessage(message);
+                    }
+                }).start();
                 break;
             case R.id.aftcome:
-                new HttpThread(url,id,"aftcome").start();
+                new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    String result = HttpDaka.DakaByPost("18538721898","aftcome");
+                    Bundle bundle = new Bundle();
+                    bundle.putString("result",result);
+                    Message message = new Message();
+                    message.setData(bundle);
+                    message.what = DAKASUCC;
+                    handler.sendMessage(message);
+                }
+            }).start();
                 break;
             case R.id.aftleave:
-                new HttpThread(url,id,"aftleave").start();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String result = HttpDaka.DakaByPost("18538721898","aftleave");
+                        Bundle bundle = new Bundle();
+                        bundle.putString("result",result);
+                        Message message = new Message();
+                        message.setData(bundle);
+                        message.what = DAKASUCC;
+                        handler.sendMessage(message);
+                    }
+                }).start();
                 break;
         }
 
